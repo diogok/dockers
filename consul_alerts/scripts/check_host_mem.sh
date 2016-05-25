a3f83b96da4bcbdffe15e0c0ba7280c540d99623
Diogo Silva <diogo@diogok.net>
2016-05-25 15:44:08 -0300

    fix  check_mem script

--- consul_alerts/scripts/check_host_mem.sh
+++ consul_alerts/scripts/check_host_mem.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-ssh ubuntu@$1 -i /opt/ops/keys/$2 \
-  "$(free -o | tail -n2 | head -n1 | awk '{ printf "%.0f", $4/($2 - $6) * 100}')"
-
-ssh ubuntu@$1 -i /opt/ops/keys/$2 \
-  "[[ $(free -o | tail -n2 | head -n1 | awk '{ printf "%.0f", $4/($2 - $6) * 100}') -lt 90 ]] && true || false"
+ssh -o StrictHostKeyChecking=no ubuntu@$1 -i /opt/ops/keys/$2 \
+  "[[ \$(free -o | tail -n2 | head -n1 | awk '{ printf \"%.0f\", \$4/(\$2 - \$6) * 100}') -lt 90 ]] && true || false" && exit 0 || exit 2
 
